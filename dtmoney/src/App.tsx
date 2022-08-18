58e6635accba6155428670623caf604ada28ccb2
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2022-08-17 23:43:11 -0300

    feature: estilizacaoes globais

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./styles/global"
 
 
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello world</h1>
     </div>
   );
