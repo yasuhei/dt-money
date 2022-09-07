3467af05bfb2ef270c71b64d60256e6efd8b9b75
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2022-09-07 12:18:18 -0300

    fix:resolvido types

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import Modal from 'react-modal'
+
+import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { FormEvent, useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { api } from '../../services/api';
 interface NewTransactionModalProps {
     isOpen: boolean;
